e5a97a7117cfb5cf2d942ce5deea10493c34d2ae
Brandon O'Hara <brandon@brandonohara.com>
2015-04-03 13:15:05 -0700

    Added fields tag, options variable now available

--- config.ff_fields.php
+++ config.ff_fields.php
@@ -1,8 +1,6 @@
 <?php
 	if(!defined("FF_FIELDS_NAME")){
-		define("FF_FIELDS_NAME", "FF Fields");
+		define("FF_FIELDS_NAME", "FreeForm Fields");
 		define("FF_FIELDS_VERSION", "1.0.0");
 		define("FF_FIELDS_EE_NAME", "Ff_fields");
 	}
-	
-	require_once("helpers/simplee.php");
--- pi.ff_fields.php
+++ pi.ff_fields.php
@@ -18,6 +18,38 @@
 			
 		}
 		
+		public function field(){
+			$field_name = explode("|", ee()->TMPL->fetch_param("name", array()) );
+			
+			if( count($field_name) == 0 )
+				return ee()->TMPL->no_results();
+				
+			$fields = array();
+			foreach($field_name as $name){
+				$query = ee()->db->where("field_name", $name)->from("freeform_fields")->get();
+				if($query->num_rows() == 0)
+					continue;
+				$field = $query->row_array();
+				
+				//settings formatting
+				$field['options'] = array();
+				$field['settings'] = json_decode($field['settings'], TRUE);
+				if(isset($field['settings']['field_list_items'])){
+					$options = explode("\n", $field['settings']['field_list_items']);
+					foreach($options as $option)
+						array_push($field['options'], array('option' => $option));
+				}
+				$field['settings'] = array($field['settings']);	
+				
+				array_push($fields, $field);
+			}
+			
+			if( count($fields) == 0 )
+				return ee()->TMPL->no_results();
+			
+			return ee()->TMPL->parse_variables(ee()->TMPL->tagdata, $fields);
+		}
+		
 		public static function usage(){
 	        return "";
 	    }
